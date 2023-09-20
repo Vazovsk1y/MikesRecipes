fa2f04726965049f5714c503cb705eeccce8d902
Vazovsk1y <anolrey.b@gmail.com>
2023-09-20 17:12:01 +0300

    -added domainServices registrator.

---
+++ Infrastructure/RandomRecipes.Domain.Services.Implementations/Extensions/Registrator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RandomRecipes.Domain.Models;
+
+namespace RandomRecipes.Domain.Services.Implementations.Extensions;
+
+public static class Registrator
+{
+	public static IServiceCollection AddDomainServices(this IServiceCollection services)
+	{
+		services.AddTransient<ICsvParser<Product>, ProductCsvParser>();
+		return services;
+	}
+}
